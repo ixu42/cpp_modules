b9d86bc4a92bdd68b3fa6e274fb519f2a3bf5cd1
Ivy Xu <xyan21418@gmail.com>
2024-05-27 13:10:30 +0300

    finished ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,16 +6,33 @@
 /*   By: ixu <ixu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 10:24:29 by ixu               #+#    #+#             */
-/*   Updated: 2024/05/23 10:24:31 by ixu              ###   ########.fr       */
+/*   Updated: 2024/05/27 13:09:18 by ixu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+static void	str_upper(char *arg)
+{
+	for (int i = 0; arg[i] != '\0'; i++)
+	{
+		if (arg[i] >= 'a' && arg[i] <= 'z')
+			arg[i] -= 32;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argv;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			str_upper(argv[i]);
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
