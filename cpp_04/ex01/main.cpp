b94d9b95280c34c345e7aeadeb3bcea2e0a30139
Ivy Xu <xyan21418@gmail.com>
2024-06-11 17:53:49 +0300

    updated the format of tests output

--- cpp_04/ex01/main.cpp
+++ cpp_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ixu <ixu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 17:51:28 by ixu               #+#    #+#             */
-/*   Updated: 2024/06/11 15:47:04 by ixu              ###   ########.fr       */
+/*   Updated: 2024/06/11 17:50:52 by ixu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 
 static void	testFromSubject()
 {
+	std::cout << "\033[36m" << "\n---Constructing objs---\n" << "\033[0m";
 	const Animal*	j = new Dog();
 	const Animal*	i = new Cat();
 
+	std::cout << "\033[36m" << "\n---Destructing objs---\n" << "\033[0m";
 	delete j; // should not create a leak
 	delete i;
 }
@@ -28,50 +30,50 @@ static void	testAnimalArray()
 	const int		animalCount = 6;
 	const Animal*	animals[animalCount];
 
-	std::cout << "\033[36m" << "\n---Constructing dogs---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Constructing dogs---\n" << "\033[0m";
 	for (int i = 0; i < animalCount / 2; i++)
 		animals[i] = new Dog();
 
-	std::cout << "\033[36m" << "\n---Constructing cats---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Constructing cats---\n" << "\033[0m";
 	for (int i = animalCount / 2; i < animalCount; i++)
 		animals[i] = new Cat();
 
-	std::cout << "\033[36m" << "\n---Animals making sounds---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Animals making sounds---\n" << "\033[0m";
 	for (int i = 0; i < animalCount; i++)
 		animals[i]->makeSound();
 
-	std::cout << "\033[36m" << "\n---Destructing objs---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Destructing objs---\n" << "\033[0m";
 	for (int i = 0; i < animalCount; i++)
 		delete animals[i];
 }
 
 static void	testDeepCopies()
 {
-	std::cout << "\033[36m" << "\n---Original cat---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Original cat---\n" << "\033[0m";
 	Cat	catOriginal;
 	catOriginal.getBrain()->addIdea("Experts suggest investing in premium wet food");
 	catOriginal.getBrain()->addIdea("Veterinarians endorse a steady supply of laser pointers");
 	catOriginal.getBrain()->addIdea("Analysts highlight the benefits of catnip investments");
-	std::cout << "\033[5;93m" << "\n---Original ideas---\n\n" << "\033[0m";
+	std::cout << "\033[5;93m" << "\n---Original ideas---\n" << "\033[0m";
 	catOriginal.getBrain()->printIdeas();
 
-	std::cout << "\033[36m" << "\n---Copy constructor---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Copy constructor---\n" << "\033[0m";
 	Cat	catCopy1(catOriginal);
-	std::cout << "\033[5;93m" << "\n---Copied ideas---\n\n" << "\033[0m";
+	std::cout << "\033[5;93m" << "\n---Copied ideas---\n" << "\033[0m";
 	catCopy1.getBrain()->printIdeas();
 
-	std::cout << "\033[36m" << "\n---Copy assignment operator---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Copy assignment operator---\n" << "\033[0m";
 	Cat	catCopy2;
 	catCopy2 = catOriginal;
-	std::cout << "\033[5;93m" << "\n---Copied ideas---\n\n" << "\033[0m";
+	std::cout << "\033[5;93m" << "\n---Copied ideas---\n" << "\033[0m";
 	catCopy2.getBrain()->printIdeas();
 
-	std::cout << "\033[36m" << "\n---Destructing objs---\n\n" << "\033[0m";
+	std::cout << "\033[36m" << "\n---Destructing objs---\n" << "\033[0m";
 }
 
 int	main()
 {
-	std::cout << "\033[96m" << "\n---TEST FROM SUBJECT---\n\n" << "\033[0m";
+	std::cout << "\033[96m" << "\n---TEST FROM SUBJECT---\n" << "\033[0m";
 	testFromSubject();
 
 	std::cout << "\033[96m" << "\n---AN ARRAY OF ANIMALS---\n" << "\033[0m";
