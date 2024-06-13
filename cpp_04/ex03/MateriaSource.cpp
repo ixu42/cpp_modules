743575d101e5e227163a09fcfc3e221a117ebacd
Ivy Xu <xyan21418@gmail.com>
2024-06-13 13:03:19 +0300

    prevented double free

--- cpp_04/ex03/MateriaSource.cpp
+++ cpp_04/ex03/MateriaSource.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MateriaSource.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ixu <ixu@student.hive.fi>                  +#+  +:+       +#+        */
+/*   By: ixu <ixu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 18:47:59 by ixu               #+#    #+#             */
-/*   Updated: 2024/06/13 11:03:27 by ixu              ###   ########.fr       */
+/*   Updated: 2024/06/13 12:50:21 by ixu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,27 @@ MateriaSource::MateriaSource(const MateriaSource& other)
 
 MateriaSource::~MateriaSource()
 {
+	void*	freedObjects[4] = {nullptr};
+	int		freedCount = 0;
+
 	for (int i = 0; i < 4; i++)
-		delete _inventory[i];
+	{
+		int	already_freed = 0;
+		for (int j = 0; j < freedCount; j++)
+		{
+			if (_inventory[i] == freedObjects[j])
+			{
+				already_freed = 1;
+				break;
+			}
+		}
+		if (!already_freed)
+		{
+			freedObjects[freedCount] = _inventory[i];
+			delete _inventory[i];
+			freedCount++;
+		}
+	}
 }
 
 MateriaSource&	MateriaSource::operator=(const MateriaSource& other)
@@ -60,12 +79,15 @@ void	MateriaSource::learnMateria(AMateria* m)
 	{
 		if (_inventory[i] == nullptr)
 		{
-			std::cout << "MateriaSource: learned " << m->getType() << std::endl;
 			_inventory[i] = m; // copy the Materia so it can be cloned later
 			return ;
 		}
 	}
-	std::cout << "MateriaSource: can know at most 4 Materias\n";
+	for (int i = 0; i < 4; i++)
+	{
+		if (_inventory[i] == m)
+			return ; // to prevent double free
+	}
 	delete m;
 }
 
